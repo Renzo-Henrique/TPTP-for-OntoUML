7322e59a92fc0bbecb20da79aa5cbfe11a842108
Renzo-Henrique <renzolealguzzo@gmail.com>
2025-09-15 23:39:37 -0300

    erro ao definir relatorType(rt01) na ontologia, axiomas originais conflitantes

--- src/common/newClassStereotypes.ts
+++ src/common/newClassStereotypes.ts
@@ -12,11 +12,11 @@ export enum ClassStereotypeRefactoredStringsForAxioms {
     ROLE_MIXIN = "roleMixin",///
     PHASE_MIXIN = "phaseMixin",///
     KIND = "kind", ///
-    COLLECTIVE = "collectiveType",
-    QUANTITY = "quantityType",
-    RELATOR = "relatorType",
-    QUALITY = "qualityType",
-    MODE = "modeType",
+    COLLECTIVE = "collectiveKind",
+    QUANTITY = "quantityKind",
+    RELATOR = "relatorKind",
+    QUALITY = "qualityKind",
+    MODE = "modeKind",
     SUBKIND = "subkind",///
     ROLE = "role",///
     PHASE = "phase",///
